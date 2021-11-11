f44d7b946a83bcbf88afad8496f7b7a422242cda
keunh <swhite.bbear@gmail.com>
2021-11-11 23:51:45 +0900

    실무 활용 - 순수 JPA와 Querydsl
    (cherry picked from commit 1ffaa58e593f3c32a941b23ba24074bb7af17bed)

--- src/main/java/study/querydsl/QuerydslApplication.java
+++ src/main/java/study/querydsl/QuerydslApplication.java
@@ -1,7 +1,11 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class QuerydslApplication {
@@ -10,4 +14,11 @@ public class QuerydslApplication {
 		SpringApplication.run(QuerydslApplication.class, args);
 	}
 
+	// JPAQueryFactory 의 동시성 문제는 EntityManager 에 의존
+	// 스프링에서 EntityManager 가 싱글톤이지만, 프록시를 주입해줘서 트랜잭션 단위로 각각 바인딩되도록 라우팅해줌
+	// 책 13.1 참고하기
+	@Bean
+	public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+		return new JPAQueryFactory(em);
+	}
 }
