ffb760555fff5618af0a11683a16db768a517bd4
keunh <swhite.bbear@gmail.com>
2021-11-14 23:33:09 +0900

    스프링 데이터 JPA가 제공하는 Querydsl 기능 - QuerydslPredicateExecutor

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
@@ -122,5 +123,36 @@ public class MemberRepositoryTest {
 
         assertThat(result.getSize()).isEqualTo(3);
         assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3");
+    }
+
+    /**
+     * QuerydslPredicateExecutor
+     * Pageable, Sort 지원
+     * 단점
+     * * 조인이 불가능하다. (묵시적 조인은 가능하지만 left join 불가능)
+     * * service 계층 등 repository 를 사용하는 클라이언트들이 predicate 를 만들어서 넘겨야 하는 의존성이 생긴다.
+     */
+    @Test
+    public void querydslPredicateExecutorTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        QMember member = QMember.member;
+        Iterable<Member> result = memberRepository.findAll(member.age.between(10, 40).and(member.username.eq("member1")));
+        for (Member findMember : result) {
+            System.out.println(findMember);
+        }
     }
 }
